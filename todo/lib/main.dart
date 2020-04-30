d715e371687a72e13d76604081fc797a0bae7c4c
Rakshith176 <58885049+Rakshith176@users.noreply.github.com>
2020-04-30 13:50:28 +0530

    user can add tasks

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,12 +1,23 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+import 'package:hive_flutter/hive_flutter.dart';
 
 
 
-void main() => runApp(MyApp());
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
+  await Hive.openBox<String>('tasks');
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -14,110 +25,144 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       title: 'TODO APP',
-      home:  ListApp()
-
+      home:ListApp(),
     );
   }
 }
 class ListApp extends StatefulWidget {
   @override
   _ListAppState createState() => _ListAppState();
+
 }
 
 class _ListAppState extends State<ListApp> {
-  Widget build(BuildContext context) {
-    DateTime now = DateTime.now();
-    String formattedDate = DateFormat(' EEE d MMM').format(now);
-    return Scaffold(
-
-        appBar: AppBar(
-          title: Text('TODO for $formattedDate',
-            style: TextStyle(color: Colors.black, fontSize: 25),),
-          centerTitle: true,
-
-        ),
-        body:
-
-        ListView(
-          children: <Widget>[
-
-            ListTile(
-              leading: Icon(Icons.access_alarms),
-              title: Text(' Warm up'),
-            ),
-            ListTile(
-              leading: Icon(Icons.chat),
-              title: Text('Check your messages'),
-            ),
-            ListTile(
-              leading: Icon(Icons.event_note),
-              title: Text('Head to events list '),
-            ),
-            ListTile(
-              leading: Icon(Icons.fastfood),
-              title: Text('Its your breakfast'),
-            ),
-
-            ListTile(
-              leading: Icon(Icons.voice_chat),
-              title: Text('Send a voice chat '),
-            ),
-            ListTile(
-              leading: Icon(Icons.shopping_cart),
-              title: Text('Shop things'),
-            ),
-            ListTile(
-              leading: Icon(Icons.add_location),
-              title: Text('Friends Lunch'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance_wallet),
-              title: Text('Pay Bills'),
-            ),
-            ListTile(
-              leading: Icon(Icons.assignment),
-              title: Text('Finish your assignments'),
-            ),
-            ListTile(
-              leading: Icon(Icons.tag_faces),
-              title: Text('Its Play Time'),
-            ),
-            ListTile(
-              leading: Icon(Icons.signal_cellular_off),
-              title: Text('Analyzing Time'),
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.chat_bubble),
-            onPressed: () {
-              showDialog(context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Messages',style: TextStyle(fontSize: 30),),
-                      content: Text('NO Messages'),
-                      actions: <Widget>[
-                        FlatButton(
-
-                          child: Text('OK'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-
-                      ],
-                    );
-                  }
-              );
-            }
-        )
+  Box<String> tasksBox;
+  TextEditingController _controller = TextEditingController();
+  TextEditingController _order= TextEditingController();
 
 
-    );
+  void initState() {
+    super.initState();
+    tasksBox = Hive.box<String>('tasks');
+
   }
 
 
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now(); //function implemented for present date to be displayed
+    String formattedDate = DateFormat(' EEE d MMM').format(now);
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('TODO for $formattedDate',
+          style: TextStyle(color: Colors.black, fontSize: 25),),
+        centerTitle: true,
+
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ValueListenableBuilder(
+                valueListenable: tasksBox.listenable(),
+                builder: (context, Box<String> tasks, _){
+                  return ListView.separated(
+                      itemBuilder: (context, index){
+                        final key = tasks.keys.toList()[index];
+                        final value = tasks.get(key);
+
+                        return ListTile(
+                          title: Text('$value', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+                          subtitle: Text('$key', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                         trailing: Row(
+                           mainAxisSize: MainAxisSize.min,
+                           children: <Widget>[
+                             IconButton(icon: Icon(Icons.delete),
+                                 onPressed:(){
+                               tasksBox.deleteAt(index);
+                             },
+                             )
+
+
+                           ],
+                         ),
+                        );
+                      },
+                      separatorBuilder:(_, index) => Divider(),
+                      itemCount: tasks.keys.toList().length
+                  );
+                },
+              )
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('ADD Tasks'),
+                  onPressed: (){
+                    showDialog(context: context,
+                        builder: (BuildContext context){
+                          return Dialog(
+                            child: Container(
+                              padding: EdgeInsets.all(30),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  TextField(
+                                    decoration: InputDecoration(hintText: 'Enter the order of the task',
+                                        suffixIcon: IconButton(
+                                          onPressed: ()=> _order.clear(),
+                                          icon: Icon(Icons.clear),)),
+                                    keyboardType: TextInputType.numberWithOptions(),
+                                    controller: _order,
+                                  ),
+                                  SizedBox(height: 15,),
+                                  TextField(
+                                    decoration: InputDecoration(hintText: 'Enter the Task',
+                                        suffixIcon: IconButton(
+                                          onPressed: ()=> _controller.clear(),
+                                          icon: Icon(Icons.clear),)),
+                                    controller: _controller,
+                                  ),
+                                  SizedBox(height: 15,),
+                                  FlatButton
+                                    (
+                                      onPressed: (){
+                                        final value=_controller.text;
+                                        final key = _order.text;
+                                        tasksBox.put(key,value);
+
+                                        Navigator.pop(context);
+
+                                      },
+                                      child: Text('ADD')
+                                  )
+
+                                ],
+                              ),
+                            ),
+                          );
+                        }
+
+                    );
+                  },
+                )
+
+
+
+              ],
+            ),
+          )
+        ],
+      ),
+
+    );
+  }
 }
+
+
+
+
 
 
 
